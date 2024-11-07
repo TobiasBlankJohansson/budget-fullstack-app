ceef292ea3bcabde632e635e8490456fdade703e
TobiasBlankJohansson <tobias@annito.se>
2024-11-07 20:58:14 +0100

    feat:add eslint and prettier

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,28 +1,42 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import pluginReactRefresh from "eslint-plugin-react-refresh";
+import eslintConfigPrettier from "eslint-config-prettier";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
+    plugins: { "react-refresh": pluginReactRefresh },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  eslintConfigPrettier,
+  {
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+      "react-refresh/only-export-components": "warn",
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-filename-extension": [
+        2,
+        { extensions: [".js", ".jsx", ".ts", ".tsx"] },
       ],
+      quotes: [2, "double", { avoidEscape: true }],
+      "no-alert": "off", // it's a simple app, alerts are fine
+      "no-shadow": "off",
+      "@typescript-eslint/no-shadow": "error",
     },
   },
-)
+];
