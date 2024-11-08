546ffb1d5159094c68b396b687a79648f0b3178d
TobiasBlankJohansson <Tobias@annito.se>
2024-11-08 11:26:37 +0100

    feat:add useState for budgetitem

--- frontend/src/components/Budget.tsx
+++ frontend/src/components/Budget.tsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import { BudgetCard } from "./BudgetCard";
+import { budgetItem } from "./item";
+
 
 
 export function Buget() {
-    
-    return (<div>
-        {BudgetCard}
-        {BudgetCard}
-        {BudgetCard}
-    </div>)
-};
+    const [needs, SetNeeds] = useState<budgetItem[]>([]);
+    const [wants, setWants] = useState<budgetItem[]>([]);
+    const [saves, setSaves] = useState<budgetItem[]>([]);
+
+
+
+  return (
+    <div>
+      <BudgetCard type={"needs"} items={needs} budget={}/>
+      <BudgetCard type={"wants"} items={wants} budget={}/>
+      <BudgetCard type={"saves"} items={saves} budget={}/>
+    </div>
+  );
+}
