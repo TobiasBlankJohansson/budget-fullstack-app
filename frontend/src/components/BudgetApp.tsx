95fbbd7ffd86d378330815518b6932c577fff35e
TobiasBlankJohansson <Tobias@annito.se>
2024-11-08 15:37:12 +0100

    feat:add mocket items for budget

--- frontend/src/components/BudgetApp.tsx
+++ frontend/src/components/BudgetApp.tsx
@@ -1,9 +1,27 @@
 import { Budget } from "./Budget";
+import { budgetItem } from "./item";
+
+const Items: budgetItem[] = [
+  {
+    id: "1",
+    name: "Stuff",
+    sum: 50,
+    type: "string",
+  },
+  {
+    id: "1",
+    name: "Stuff",
+    sum: 50,
+    type: "string",
+  },
+];
 
 export function BudgetApp() {
-    return (<>
-    <main>
-        <Budget />
-    </main>
-    </>)
-};
+  return (
+    <>
+      <main>
+        <Budget income={100} items={Items} />
+      </main>
+    </>
+  );
+}
