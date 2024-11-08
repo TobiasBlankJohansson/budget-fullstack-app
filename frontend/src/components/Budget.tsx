14e8ada9f3d1a8e6eba217f32911799f4896f826
TobiasBlankJohansson <Tobias@annito.se>
2024-11-08 11:35:58 +0100

    feat:add useeffect to set usestate items for the diffrent cards

--- frontend/src/components/Budget.tsx
+++ frontend/src/components/Budget.tsx
@@ -1,21 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BudgetCard } from "./BudgetCard";
 import { budgetItem } from "./item";
 
+type inputBudget = {
+  items: budgetItem[];
+  income: number;
+};
 
+export function Buget({ items, income }: inputBudget) {
+  const [needs, setNeeds] = useState<budgetItem[]>([]);
+  const [wants, setWants] = useState<budgetItem[]>([]);
+  const [saves, setSaves] = useState<budgetItem[]>([]);
 
-export function Buget() {
-    const [needs, SetNeeds] = useState<budgetItem[]>([]);
-    const [wants, setWants] = useState<budgetItem[]>([]);
-    const [saves, setSaves] = useState<budgetItem[]>([]);
+  useEffect(() => {
+    setNeeds(() => []);
+    setWants(() => []);
+    setSaves(() => []);
 
-
+    items.forEach((item) => {
+      const itemType: string = item.type;
+      if (itemType == "needs") {
+        setNeeds((prev) => [...prev, item]);
+        return;
+      }
+      if (itemType == "wants") {
+        setWants((prev) => [...prev, item]);
+        return;
+      }
+      if (itemType == "saves") {
+        setSaves((prev) => [...prev, item]);
+        return;
+      }
+    });
+  }, items);
 
   return (
     <div>
-      <BudgetCard type={"needs"} items={needs} budget={}/>
-      <BudgetCard type={"wants"} items={wants} budget={}/>
-      <BudgetCard type={"saves"} items={saves} budget={}/>
+      <BudgetCard type={"needs"} items={needs} budget={} />
+      <BudgetCard type={"wants"} items={wants} budget={} />
+      <BudgetCard type={"saves"} items={saves} budget={} />
     </div>
   );
 }
