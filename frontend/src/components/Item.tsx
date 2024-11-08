58c1c62326226fed3288455c48465484528acbbf
TobiasBlankJohansson <Tobias@annito.se>
2024-11-08 09:50:27 +0100

    feat:add return table rows in item

--- frontend/src/components/Item.tsx
+++ frontend/src/components/Item.tsx
@@ -1,10 +1,15 @@
 type bugetItem = {
-    id: string;
-    name: string;
-    sum: string;
-    type: string;
-}
-
-export function item() {
-    
+  id: string;
+  name: string;
+  sum: string;
+  type: string;
 };
+
+export function item({ id, name, sum }: bugetItem) {
+  return (
+    <tr key={id}>
+      <th>{name}</th>
+      <th>{sum}</th>
+    </tr>
+  );
+}
