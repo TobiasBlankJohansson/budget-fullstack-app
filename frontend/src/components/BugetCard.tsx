844f78eab2509af9d1b434e26ab1aead43951325
TobiasBlankJohansson <Tobias@annito.se>
2024-11-08 10:28:59 +0100

    feat:add canSpend and totalSpend calculated in bugetCard

--- frontend/src/components/BugetCard.tsx
+++ frontend/src/components/BugetCard.tsx
@@ -1,18 +1,31 @@
+import { useEffect, useState } from "react";
 import { bugetItem, Item } from "./item";
 
 type inputBugetCard = {
   type: string;
   items: bugetItem[];
+  bugent: number;
 };
 
-export function BugetCard({ type, items }: inputBugetCard) {
+export function BugetCard({ type, items, bugent }: inputBugetCard) {
+  const [canSpend, setCanSpend] = useState<number>(0);
+  const [totalSpend, setTotalSpend] = useState<number>(0);
+
+  useEffect(() => {
+    let spent: number = 0;
+    items.forEach((item) => (spent += item.sum));
+
+    setCanSpend(() => bugent - spent);
+    setTotalSpend(() => spent);
+  }, items);
+
   return (
     <section>
       <header>
         <h3>{type}</h3>
       </header>
       <article>
-        <p>Can spend: {}</p>
+        <p>Can spend: {canSpend}</p>
         <table>
           <tr>
             <td>Item</td>
@@ -20,7 +33,7 @@ export function BugetCard({ type, items }: inputBugetCard) {
           </tr>
           {items.map((item) => Item(item))}
         </table>
-        <p>Total spent: {}</p>
+        <p>Total spent: {totalSpend}</p>
       </article>
     </section>
   );
