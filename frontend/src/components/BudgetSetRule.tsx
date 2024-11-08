913dab67e426254cd7e47a7360c9b306d7832f1d
TobiasBlankJohansson <Tobias@annito.se>
2024-11-08 15:30:44 +0100

    feat:add logic for handeling submit for form

--- frontend/src/components/BudgetSetRule.tsx
+++ frontend/src/components/BudgetSetRule.tsx
@@ -2,20 +2,56 @@ import { useState } from "react";
 
 type inportBudgetSetRule = {
   budget: number;
+  onSaveClick: (needs: number, whants: number, saves: number) => void;
 };
 
-export function BudgetSetRule({ budget }: inportBudgetSetRule) {
-  const [needs, setNeeds] = useState<number>(50);
-  const [wants, setWants] = useState<number>(30);
+type formData = {
+  needs: number;
+  whants: number;
+};
+
+export function BudgetSetRule({ budget, onSaveClick }: inportBudgetSetRule) {
+  const [formData, setFormData] = useState<formData>({ needs: 50, whants: 30 });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handelSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const saves = 100 - formData.needs - formData.whants;
+    if (saves < 0) {
+      //todo: add if negetive
+      return;
+    }
+
+    onSaveClick(
+      (formData.needs * budget) / 100,
+      (formData.whants * budget) / 100,
+      (saves * budget) / 100
+    );
+  };
 
   return (
     <>
-      <form action="">
+      <form onSubmit={handelSubmit}>
         <label>Needs</label>
-        <input></input>
+        <input
+          type="number"
+          value={formData.needs}
+          onChange={handleChange}
+        ></input>
         <label>Whants</label>
-        <input></input>
-        <button onClick={}>Save</button>
+        <input
+          type="number"
+          value={formData.whants}
+          onChange={handleChange}
+        ></input>
+        <button type="submit">Save</button>
       </form>
     </>
   );
