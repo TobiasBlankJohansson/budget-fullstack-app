406dc76c5ec259069da91a61df7dbe8bf3c52ba5
TobiasBlankJohansson <Tobias@annito.se>
2024-11-08 13:13:49 +0100

    feat:add html for BudgetSetRule

--- frontend/src/components/BudgetSetRule.tsx
+++ frontend/src/components/BudgetSetRule.tsx
@@ -1,7 +1,22 @@
-type inportBudgetSetRule = {
-    budget: number;
-}
+import { useState } from "react";
 
-export function BudgetSetRule({budget}: inportBudgetSetRule) {
-    
+type inportBudgetSetRule = {
+  budget: number;
 };
+
+export function BudgetSetRule({ budget }: inportBudgetSetRule) {
+  const [needs, setNeeds] = useState<number>(50);
+  const [wants, setWants] = useState<number>(30);
+
+  return (
+    <>
+      <form action="">
+        <label>Needs</label>
+        <input></input>
+        <label>Whants</label>
+        <input></input>
+        <button onClick={}>Save</button>
+      </form>
+    </>
+  );
+}
