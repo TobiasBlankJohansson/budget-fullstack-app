dfdbc687fa3faeba5fe2a251b988aa6c951dd143
TobiasBlankJohansson <Tobias@annito.se>
2024-11-08 10:17:23 +0100

    feat:add inputBugetCard type and html in bugetcard

--- frontend/src/components/BugetCard.tsx
+++ frontend/src/components/BugetCard.tsx
@@ -1,10 +1,27 @@
 import { bugetItem, Item } from "./item";
 
+type inputBugetCard = {
+  type: string;
+  items: bugetItem[];
+};
 
-
-export function BugetCard({ id, name, sum, type }: bugetItem) {
-  return (<section>
-    <h3>{type}</h3>
-    <p>Can spend</p>
-  </section>)
+export function BugetCard({ type, items }: inputBugetCard) {
+  return (
+    <section>
+      <header>
+        <h3>{type}</h3>
+      </header>
+      <article>
+        <p>Can spend: {}</p>
+        <table>
+          <tr>
+            <td>Item</td>
+            <td></td>
+          </tr>
+          {items.map((item) => Item(item))}
+        </table>
+        <p>Total spent: {}</p>
+      </article>
+    </section>
+  );
 }
