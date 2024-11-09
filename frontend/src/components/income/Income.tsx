b2abfcb11bd2ecfcbff702b47c5206f21cf32d7c
TobiasBlankJohansson <tobias@annito.se>
2024-11-09 17:59:36 +0100

    feat:add Income component with its input type

---
+++ frontend/src/components/income/Income.tsx
@@ -0,0 +1,9 @@
+import { budgetItem } from "../Item";
+
+type inputIncome = {
+  items: budgetItem[];
+  setItems: React.Dispatch<React.SetStateAction<budgetItem[]>>;
+  setIncome: React.Dispatch<React.SetStateAction<number>>;
+};
+
+export function Income({ items, setItems, setIncome }: inputIncome) {}
