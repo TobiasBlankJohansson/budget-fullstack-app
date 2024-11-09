8e3d46bdcfd1bd5e8b2a407d66be5b35a2fcf32f
TobiasBlankJohansson <tobias@annito.se>
2024-11-09 14:03:49 +0100

    feat:add handelSubmit with logic to get input values

--- frontend/src/components/expenses/AddItemForm.tsx
+++ frontend/src/components/expenses/AddItemForm.tsx
@@ -1,10 +1,20 @@
 export function AddItemForm() {
+  const handelSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    const item: HTMLInputElement = document.getElementById(
+      "add-item__item"
+    ) as HTMLInputElement;
+    const amount: HTMLInputElement = document.getElementById(
+      "add-item__add"
+    ) as HTMLInputElement;
+  };
+
   return (
-    <form onSubmit={() => {}}>
+    <form onSubmit={handelSubmit}>
       <label>Item</label>
       <input type="text" id="add-item__item" required />
       <label>Amount</label>
-      <input type="number" id="add-item__add" required />
+      <input type="number" id="add-item__add" min={1} required />
       <button type="submit">Add</button>
     </form>
   );
