45bd4f8f84c7148a16fac064294501560c4d7298
TobiasBlankJohansson <tobias@annito.se>
2024-11-09 11:58:16 +0100

    feat:add handelSubmit with it's logic

--- frontend/src/components/BudgetSetRule.tsx
+++ frontend/src/components/BudgetSetRule.tsx
@@ -11,15 +11,31 @@ type formData = {
 };
 
 export function BudgetSetRule({ setCardItems }: inputBudgetSetRule) {
+  const handelSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    const needs: number = document.getElementById("needs").value;
+    const wants: number = document.getElementById("wants").value;
+    if (needs == null || wants == null) {
+      return;
+      //todo:give user response
+    }
 
-  
+    const saves: number = 100 - needs - wants;
+    if (saves < 0) {
+      return;
+      //todo:give user response
+    }
+
+    setCardItems(() => ({ needs: needs, wants: wants, saves: saves }));
+  };
+
   return (
     <>
       <form onSubmit={handelSubmit}>
         <label>Needs</label>
-        <input type="number" id="needs"></input>
+        <input type="number" id="needs" required></input>
         <label>Whants</label>
-        <input type="number" id="whants"></input>
+        <input type="number" id="whants" required></input>
         <button type="submit">Save</button>
       </form>
     </>
