4a483f77b9fd2fb329880e5c22530273a76de12e
TobiasBlankJohansson <tobias@annito.se>
2024-11-09 11:40:47 +0100

    refa:refactor BudgetSetRule by removing all code

--- frontend/src/components/BudgetSetRule.tsx
+++ frontend/src/components/BudgetSetRule.tsx
@@ -10,41 +10,17 @@ type formData = {
 };
 
 export function BudgetSetRule({onSaveClick }: inportBudgetSetRule) {
-  const [formData, setFormData] = useState<formData>({ needs: 50, whants: 30 });
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
-
-  const handelSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const saves = 100 - formData.needs - formData.whants;
-    if (saves < 0) {
-      //todo: add if negetive
-      return;
-    }
-
-    onSaveClick(formData.needs, formData.whants, saves);
-  };
-
+  
   return (
     <>
       <form onSubmit={handelSubmit}>
         <label>Needs</label>
         <input
           type="number"
-          value={formData.needs}
-          onChange={handleChange}
         ></input>
         <label>Whants</label>
         <input
           type="number"
-          value={formData.whants}
-          onChange={handleChange}
         ></input>
         <button type="submit">Save</button>
       </form>
