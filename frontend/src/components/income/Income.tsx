1c84aefb67f8167a3f8cb0a191ebca680f158e8b
TobiasBlankJohansson <tobias@annito.se>
2024-11-09 18:15:04 +0100

    feat:add DisplayIncomeList to Income

--- frontend/src/components/income/Income.tsx
+++ frontend/src/components/income/Income.tsx
@@ -1,4 +1,6 @@
+import { useEffect } from "react";
 import { budgetItem } from "../Item";
+import { DisplayIncomeList } from "./DisplayIncomeList";
 
 type inputIncome = {
   items: budgetItem[];
@@ -7,5 +9,11 @@ type inputIncome = {
 };
 
 export function Income({ items, setItems, setIncome }: inputIncome) {
-  return <></>;
+
+
+  return (
+    <section>
+      <DisplayIncomeList items={items} setItems={setItems} totalIncome={100} />
+    </section>
+  );
 }
