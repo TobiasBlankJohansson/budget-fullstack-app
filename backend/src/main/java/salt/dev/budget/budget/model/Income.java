3b69b3fb43fd8c9bb8f56102cf232d18c5ff8efd
TobiasBlankJohansson <tobias@annito.se>
2024-11-10 14:37:22 +0100

    feat:add entity and fields to income

--- backend/src/main/java/salt/dev/budget/budget/model/Income.java
+++ backend/src/main/java/salt/dev/budget/budget/model/Income.java
@@ -1,4 +1,22 @@
 package salt.dev.budget.budget.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "income")
 public class Income {
-}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private long sum;
+    @Column(nullable = false)
+    private String type;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name="budget_id")
+    private Budget budget;}
