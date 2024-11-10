6f6bb9d0a10665f19960023c673e89bf6e0a9184
TobiasBlankJohansson <tobias@annito.se>
2024-11-10 17:02:36 +0100

    feat:add service to BudgetController

--- backend/src/main/java/salt/dev/budget/budget/controller/BudgetController.java
+++ backend/src/main/java/salt/dev/budget/budget/controller/BudgetController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import salt.dev.budget.budget.model.Income;
+import salt.dev.budget.budget.service.BudgetService;
 
 import java.util.List;
 
@@ -15,9 +16,11 @@ import java.util.List;
 public class BudgetController {
 
     private final String BUDGET_PATH;
+    private final BudgetService service;
 
-    public BudgetController(@Value("${api.budget.path}")String BUDGET_PATH) {
+    public BudgetController(@Value("${api.budget.path}")String BUDGET_PATH, BudgetService service) {
         this.BUDGET_PATH = BUDGET_PATH;
+        this.service = service;
     }
 
     @GetMapping("{id}/income")
