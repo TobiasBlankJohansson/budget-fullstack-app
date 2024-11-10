2081e8398a4b1604137e4594303e9ec4d70f1294
TobiasBlankJohansson <tobias@annito.se>
2024-11-10 17:01:19 +0100

    feat:add getIncomeList boiler plate code

--- backend/src/main/java/salt/dev/budget/budget/controller/BudgetController.java
+++ backend/src/main/java/salt/dev/budget/budget/controller/BudgetController.java
@@ -1,8 +1,14 @@
 package salt.dev.budget.budget.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import salt.dev.budget.budget.model.Income;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/budget/")
@@ -14,5 +20,8 @@ public class BudgetController {
         this.BUDGET_PATH = BUDGET_PATH;
     }
 
-
+    @GetMapping("{id}/income")
+    public ResponseEntity<List<Income>> getIncomeList(@PathVariable long id){
+        return null;
+    }
 }
