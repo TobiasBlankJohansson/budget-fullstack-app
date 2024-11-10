57874f38955a27bd1522a81577bdf8bc01e6061a
TobiasBlankJohansson <tobias@annito.se>
2024-11-10 21:14:56 +0100

    feat:add addIncome function

--- frontend/src/api/income.ts
+++ frontend/src/api/income.ts
@@ -2,8 +2,28 @@ import { budgetItem } from "../components/Item";
 
 const path = import.meta.env.BUDGET_PATH;
 
-export async function getIncome(id: number): Promise<budgetItem[]> {
-  var response = await fetch(`${path}/${id}`);
+export async function getIncome(budgetId: number): Promise<budgetItem[]> {
+  var response = await fetch(`${path}/${budgetId}`);
+  if (!response.ok) {
+    throw new Error();
+  }
+
+  return response.json();
+}
+
+export async function addIncome(
+  budgetId: number,
+  name: string,
+  sum: number,
+  type: string
+): Promise<budgetItem[]> {
+  var response = await fetch(`${path}/${budgetId}`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/JSON",
+    },
+    body: JSON.stringify({ budgetId, name, sum, type }),
+  });
   if (!response.ok) {
     throw new Error();
   }
