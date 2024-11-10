de232189122dbc6fb90cb8ea75e8534b0e2fcd51
TobiasBlankJohansson <tobias@annito.se>
2024-11-11 00:46:10 +0100

    feat:add logic removeExpense and removeIncome

--- backend/src/main/java/salt/dev/budget/budget/controller/BudgetController.java
+++ backend/src/main/java/salt/dev/budget/budget/controller/BudgetController.java
@@ -17,43 +17,45 @@ public class BudgetController {
     private final String BUDGET_PATH;
     private final BudgetService service;
 
-    public BudgetController(@Value("${api.budget.path}")String BUDGET_PATH, BudgetService service) {
+    public BudgetController(@Value("${api.budget.path}") String BUDGET_PATH, BudgetService service) {
         this.BUDGET_PATH = BUDGET_PATH;
         this.service = service;
     }
 
     @GetMapping("{id}/income")
-    public ResponseEntity<List<Income>> getIncomeList(@PathVariable long id){
+    public ResponseEntity<List<Income>> getIncomeList(@PathVariable long id) {
         return ResponseEntity.ok(service.getIncomeList(id));
     }
 
     @PostMapping("{id}/income")
     public ResponseEntity<Income> addIncomeList(
-            @PathVariable long id, @RequestBody RequestIncomeDto incomeDto){
+            @PathVariable long id, @RequestBody RequestIncomeDto incomeDto) {
         return ResponseEntity.accepted().body(
-                service.addIncome(id,incomeDto.name(),incomeDto.sum(),incomeDto.type()));
+                service.addIncome(id, incomeDto.name(), incomeDto.sum(), incomeDto.type()));
     }
 
     @DeleteMapping("{budgetId}/expense/{incomeId}")
-    public ResponseEntity<Void> removeIncome(@PathVariable long budgetId, @PathVariable long incomeId){
-        return null;
+    public ResponseEntity<Void> removeIncome(@PathVariable long budgetId, @PathVariable long incomeId) {
+        service.removeIncome(budgetId, incomeId);
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping("{id}/expense")
-    public ResponseEntity<List<Expense>> getExpenseList(@PathVariable long id){
+    public ResponseEntity<List<Expense>> getExpenseList(@PathVariable long id) {
         return ResponseEntity.ok(service.getExpenseList(id));
     }
 
     @PostMapping("{id}/expense")
     public ResponseEntity<Expense> addExpenseList(
-            @PathVariable long id, @RequestBody RequestIncomeDto incomeDto){
+            @PathVariable long id, @RequestBody RequestIncomeDto incomeDto) {
         return ResponseEntity.accepted().body(
-                service.addExpense(id,incomeDto.name(),incomeDto.sum(),incomeDto.type()));
+                service.addExpense(id, incomeDto.name(), incomeDto.sum(), incomeDto.type()));
     }
 
     @DeleteMapping("{budgetId}/expense/{expenseId}")
-    public ResponseEntity<Void> removeExpense(@PathVariable long budgetId, @PathVariable long expenseId){
-        return null;
+    public ResponseEntity<Void> removeExpense(@PathVariable long budgetId, @PathVariable long expenseId) {
+        service.removeExpense(budgetId, expenseId);
+        return ResponseEntity.ok().build();
     }
 
 }
