942e6017f1fb76951649eb7b4c488b4aab94cd82
TobiasBlankJohansson <tobias@annito.se>
2024-11-10 13:54:41 +0100

    feat:add backend

---
+++ backend/src/test/java/salt/dev/budget/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package salt.dev.budget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
