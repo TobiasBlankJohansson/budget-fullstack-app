281029dd608f6296e76df12c69e5b723032453c1
TobiasBlankJohansson <tobias@annito.se>
2024-11-10 14:18:19 +0100

    feat:add final BUDGET_PATH varible

--- backend/src/main/java/salt/dev/budget/budget/controller/BudgetController.java
+++ backend/src/main/java/salt/dev/budget/budget/controller/BudgetController.java
@@ -1,5 +1,6 @@
 package salt.dev.budget.budget.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/budget/")
 public class BudgetController {
 
+    private final String BUDGET_PATH;
 
-
+    public BudgetController(@Value("${api.budget.path}")String BUDGET_PATH) {
+        this.BUDGET_PATH = BUDGET_PATH;
+    }
 }
