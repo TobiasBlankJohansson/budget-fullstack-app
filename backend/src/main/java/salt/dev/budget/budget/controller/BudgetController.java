a240d6586661bf295cf171bec38293a9e11d13d9
TobiasBlankJohansson <tobias@annito.se>
2024-11-10 14:14:19 +0100

    feat:add RestController to budget

--- backend/src/main/java/salt/dev/budget/budget/controller/BudgetController.java
+++ backend/src/main/java/salt/dev/budget/budget/controller/BudgetController.java
@@ -1,4 +1,12 @@
 package salt.dev.budget.budget.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/budget/")
 public class BudgetController {
+
+
+
 }
