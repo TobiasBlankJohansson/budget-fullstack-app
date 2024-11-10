0d1c82bd69bcf9d77992ddab1be21ee310f2a0a3
TobiasBlankJohansson <tobias@annito.se>
2024-11-10 17:04:05 +0100

    feat:add BudgetService to be a bean with a repo

--- backend/src/main/java/salt/dev/budget/budget/service/BudgetService.java
+++ backend/src/main/java/salt/dev/budget/budget/service/BudgetService.java
@@ -1,7 +1,15 @@
 package salt.dev.budget.budget.service;
 
-public class BudgetService {
+import org.springframework.stereotype.Service;
+import salt.dev.budget.budget.repository.BudgetRepository;
 
+@Service
+public class BudgetService {
+    private final BudgetRepository repo;
+
+    public BudgetService(BudgetRepository repo) {
+        this.repo = repo;
+    }
 
 
 }
