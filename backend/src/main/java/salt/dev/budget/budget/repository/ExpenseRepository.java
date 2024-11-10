0b579d27b9341a884a658788645c9ffe2bc85a95
TobiasBlankJohansson <tobias@annito.se>
2024-11-10 23:03:20 +0100

    feat:add ExpenseRepository

---
+++ backend/src/main/java/salt/dev/budget/budget/repository/ExpenseRepository.java
@@ -0,0 +1,13 @@
+package salt.dev.budget.budget.repository;
+
+import org.springframework.data.repository.ListCrudRepository;
+import salt.dev.budget.budget.model.Budget;
+import salt.dev.budget.budget.model.Expense;
+import salt.dev.budget.budget.model.Income;
+
+import java.util.List;
+
+public interface ExpenseRepository extends ListCrudRepository<Expense,Long> {
+    List<Income> findAllByBudget(Budget budget);
+
+}
