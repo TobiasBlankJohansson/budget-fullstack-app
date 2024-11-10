cb162d4dcd9b362390094af34a3d76315b9c7181
TobiasBlankJohansson <tobias@annito.se>
2024-11-10 18:46:47 +0100

    feat:add getIncome fetch

---
+++ frontend/src/api/income.ts
@@ -0,0 +1,11 @@
+import { budgetItem } from "../components/Item";
+
+const path = import.meta.env.BUDGET_PATH;
+
+export async function getIncome(id:number): Promise<budgetItem[]> {
+    var response = await fetch(`${path}/${id}`);
+    if(!response.ok){
+        throw new Error();
+    }
+    return response.json();
+}
