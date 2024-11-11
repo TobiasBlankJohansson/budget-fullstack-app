8a1c9ba4d18cfb87f35ceaf1ca332bead4185f8f
TobiasBlankJohansson <Tobias@annito.se>
2024-11-11 09:36:21 +0100

    feat:add budgetId  then fetch it

--- frontend/src/components/BudgetApp.tsx
+++ frontend/src/components/BudgetApp.tsx
@@ -5,17 +5,21 @@ import { DisplayIncome } from "./income/DisplayIncome";
 import { Income } from "./income/Income";
 import { getIncome } from "../api/income";
 import { getExpense } from "../api/expense";
+import { addBudget } from "../api/budget";
 
 export function BudgetApp() {
+  const [budgetId, setBudgetId] = useState<number>(0);
   const [budgetItem, setBudgetItem] = useState<budgetItem[]>([]);
   const [incomeItem, setIncomeItem] = useState<budgetItem[]>([]);
   const [income, setIncome] = useState<number>(100);
 
   useEffect(() => {
     const getList = async () => {
-      const incomeList = await getIncome(2000);
+      const id = await addBudget(2000);
+      setBudgetId(() => id);
+      const incomeList = await getIncome(budgetId);
       setIncomeItem(() => incomeList);
-      const expenseList = await getExpense(2000);
+      const expenseList = await getExpense(budgetId);
       setBudgetItem(() => expenseList);
     };
     getList();
